da5c320bb7e24f1ee4fd66e3675c56b98f3ce935
Nidhushan Kanagaraja <42415753+Nidhushan@users.noreply.github.com>
2024-02-13 17:25:55 -0500

    Bug fixes for the boss

--- 2d souls/Assets/Scripts/Boss_N/Boss_run.cs
+++ 2d souls/Assets/Scripts/Boss_N/Boss_run.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Boss_run : StateMachineBehaviour
 {
@@ -8,18 +9,22 @@ public class Boss_run : StateMachineBehaviour
     Transform player;
     Rigidbody2D rb;
     public float attackRange = 3f;
+    public float desiredSpeed = 3f;
+    AIPath aiPath;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponentInParent<Rigidbody2D>();
+        aiPath = animator.GetComponentInParent<AIPath>();
+        aiPath.maxSpeed = desiredSpeed;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Vector3.Distance(player.position, rb.position) <= attackRange)
+        if (Vector3.Distance(player.position, rb.position) < attackRange)
         {
 
             animator.SetTrigger("Attack start");
--- 2d souls/Assets/Scripts/Boss_N/EnemyGFX.cs
+++ 2d souls/Assets/Scripts/Boss_N/EnemyGFX.cs
@@ -9,29 +9,26 @@ public class EnemyGFX : MonoBehaviour
     SpriteRenderer spriteRenderer;
     BoxCollider2D boxCollider;
     CircleCollider2D circleCollider;
-    
+
+
     void Start()
     {
-        // Get the SpriteRenderer component attached to the same GameObject
         spriteRenderer = GetComponent<SpriteRenderer>();
-        boxCollider = GetComponentInParent<BoxCollider2D>();
         circleCollider = GetComponentInParent<CircleCollider2D>();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Transform parentTransform = transform.parent;
         if (aiPath.desiredVelocity.x >= 0.01)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-            //boxCollider.offset = new Vector3(-0.93f, 0.38f);
-            circleCollider.offset = new Vector3(-1f, 0.30f);
+            parentTransform.localScale = new Vector3(-1f, 1f, 1f);
         }
         else if(aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-            //boxCollider.offset = new Vector3(0.15f, 0.44f);
-            circleCollider.offset = new Vector3(0f, 0.3f);
+            parentTransform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
+
 }
