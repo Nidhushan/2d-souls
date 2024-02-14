154ed4453eb153ac284c8c0d7ae7dc431ac11cd4
Kewl <87680132+ac8736@users.noreply.github.com>
2024-02-13 23:52:04 -0500

    changes

--- 2d souls/Assets/Scripts/Boss_N/Boss_run.cs
+++ 2d souls/Assets/Scripts/Boss_N/Boss_run.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 
 public class Boss_run : StateMachineBehaviour
 {
-
+    public float AttackRange = 2.0f;
     Transform player;
     Rigidbody2D rb;
     public float attackRange = 3f;
+    public float speed = 2.5f;
+    BringerOfDeath boss;
+    BringerOfDeathWeapon weapon;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponentInParent<Rigidbody2D>();
+        boss = animator.GetComponentInParent<BringerOfDeath>();
+        weapon = animator.GetComponent<BringerOfDeathWeapon>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Vector3.Distance(player.position, rb.position) <= attackRange)
+        boss.Flip();
+        Vector2 target = new(player.position.x, rb.position.y);
+
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.position, rb.position) <= AttackRange && weapon.canAttack && boss.health > 0)
         {
-
-            animator.SetTrigger("Attack start");
-
+            animator.SetTrigger("Attack");
         }
 
+        if (boss.health <= 0)
+        {
+            animator.SetTrigger("Die");
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Attack start");
+        animator.ResetTrigger("Attack");
     }
 }
--- 2d souls/Assets/Scripts/BringerOfDeath_Scripts/BringerOfDeath_Move.cs
+++ 2d souls/Assets/Scripts/BringerOfDeath_Scripts/BringerOfDeath_Move.cs
@@ -5,51 +5,45 @@ using UnityEngine;
 public class BringerOfDeath_Move : StateMachineBehaviour
 {
     public float speed = 1.0f;
-    public float AttackRange = 5.0f;
-    Transform player;
-    Rigidbody2D rb;
+    public float AttackRange = 3.0f;
+    private Transform player;
+    private Rigidbody2D rb;
     BringerOfDeath boss;
-
+    BringerOfDeathWeapon weapon;
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       player = GameObject.FindGameObjectWithTag("Player").transform;
-       rb = animator.GetComponent<Rigidbody2D>();
-       boss = animator.GetComponent<BringerOfDeath>();
-       
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<BringerOfDeath>();
+        weapon = animator.GetComponent<BringerOfDeathWeapon>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        boss.LookAtPlayer();
+        boss.Flip();
 
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 target = new(player.position.x, rb.position.y);
+
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
 
-        if (Vector2.Distance(player.position, rb.position)<AttackRange)
+        if (Vector2.Distance(player.position, rb.position) <= AttackRange && weapon.canAttack && boss.health > 0)
         {
             animator.SetTrigger("Attack");
+        }
+
+        if (boss.health <= 0)
+        {
+            animator.SetTrigger("Death");
         }
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Attack");
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
